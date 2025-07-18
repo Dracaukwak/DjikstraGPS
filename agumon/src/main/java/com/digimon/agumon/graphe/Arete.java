8f220d3568c6d8d5c195c188f2c1a271888a3631
Dracaukwak <43927819+Dracaukwak@users.noreply.github.com>
2025-07-18 17:18:35 +0200

    Ajout de getteur sur arete et update

--- agumon/src/main/java/com/digimon/agumon/graphe/Arete.java
+++ agumon/src/main/java/com/digimon/agumon/graphe/Arete.java
@@ -1,13 +1,63 @@
 package com.digimon.agumon.graphe;
 
+import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
+
+import java.util.ArrayList;
+
 public class Arete {
-    private Sommet a,b;
+    private Sommet a, b;
     private double distance;
+    private Line line;
+    private Text texteDistance;
+
 
     public Arete(Sommet a, Sommet b, double distance) {
         this.a = a;
         this.b = b;
         this.distance = distance;
+    }
+
+    static public Arete getArete(ArrayList<Arete> aretes,Sommet a, Sommet b){
+        for (Arete ar : aretes){
+            if((ar.getA().equals(a) && ar.getB().equals(b) )|| (ar.getA().equals(b) && ar.getB().equals(a))){
+                return ar;
+            }
+        }
+        System.out.println("Arête pas trouvée");
+        return null;
+    }
+
+    static public void updateLine(Sommet a, Sommet b,Line line) {
+        line.setStartX(a.getCercle().getCenterX());
+        line.setStartY(a.getCercle().getCenterY());
+        line.setEndX(b.getCercle().getCenterX());
+        line.setEndY(b.getCercle().getCenterY());
+    }
+
+    static public void updateAllLines(ArrayList<Arete> aretes){
+        for(Arete ar : aretes){
+            Sommet a = ar.getA();
+            Sommet b = ar.getB();
+            updateLine(a,b,ar.getLine());
+        }
+    }
+
+
+    public Text getTexteDistance() {
+        return texteDistance;
+    }
+
+    public void setTexteDistance(Text texteDistance) {
+        this.texteDistance = texteDistance;
+    }
+
+    public Line getLine() {
+        return line;
+    }
+
+    public void setLine(Line line) {
+        this.line = line;
     }
 
     public Sommet getA() {
