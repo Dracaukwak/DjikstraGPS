666eeecbb144928b989f9a2707689dbb21f30238
Dracaukwak <43927819+Dracaukwak@users.noreply.github.com>
2025-07-18 17:19:52 +0200

    ajout chargeur planete et chargeur solution

--- agumon/src/main/java/com/digimon/agumon/graphe/Chargeur.java
+++ agumon/src/main/java/com/digimon/agumon/graphe/Chargeur.java
@@ -1,5 +1,7 @@
 package com.digimon.agumon.graphe;
 
+import javafx.scene.paint.Color;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -7,7 +9,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Chargeur {
-
 
     /**
      * Charge les sommets du fichier dans une liste
@@ -44,11 +45,13 @@ public class Chargeur {
 
     /**
      * Charge les aretes du fichier dans une liste
+     * les arêtes sont créées en recherchant au préalable leur nom dans la liste des sommets
+     * pour obtenir la même intance !!!
      *
      * @param cheminFichier chemin contenant les aretes
      * @return la liste des aretes
      */
-    static public ArrayList<Arete> chargerAreteDepuisFichier(String cheminFichier) {
+    static public ArrayList<Arete> chargerAreteDepuisFichier(String cheminFichier, ArrayList<Sommet> sommets) {
         ArrayList<Arete> aretes = new ArrayList<>();
         List<String> lignes;
         try {
@@ -66,8 +69,8 @@ public class Chargeur {
             String nomU = parties[0];
             String nomV = parties[1];
             double distance = Double.parseDouble(parties[2]);
-            Sommet u = new Sommet(nomU);
-            Sommet v = new Sommet(nomV);
+            Sommet u = Sommet.getSommetAvecNom(sommets, nomU);
+            Sommet v = Sommet.getSommetAvecNom(sommets, nomV);
             Arete a = new Arete(u, v, distance);
             aretes.add(a);
         }
@@ -75,39 +78,59 @@ public class Chargeur {
     }
 
     /**
-     * Charge les resultats du fichier dans une liste
+     * Charge les resultats du fichier dans une liste d'arêtes
      * Format du fichier :
      * 3
      * LA
      * LA 613 SF
      * LA 4497 NY
+     *
      * @param cheminFichierSolution
      * @return
      */
-    public static ArrayList<Resultat> chargerSolutionDepuisFichier(String cheminFichierSolution){
-        ArrayList<Resultat> resultats = new ArrayList<>();
+    public static ArrayList<Arete> chargerSolutionDepuisFichier(String cheminFichierSolution, ArrayList<Sommet> sommets, ArrayList<Arete> aretes) {
+        ArrayList<Arete> areteResultats = new ArrayList<>();
         List<String> lignes;
-        try{
+        try {
             lignes = Files.readAllLines(Paths.get(cheminFichierSolution));
         } catch (IOException e) {
             throw new IllegalArgumentException("Erreur : chargerSolutionDepuisFichier");
         }
         int nbVilles = Integer.parseInt(lignes.getFirst());
         String villeSource = lignes.get(1); // Si jamais besoin
-
-        for(int i = 2; i <= nbVilles; i++){
+        for (int i = 2; i <= nbVilles; i++) {
             String[] parties = lignes.get(i).split(" ");
-            String villeA = parties[0];
-            Sommet a = new Sommet(villeA);
-
+            String nomU = parties[0];
             double distance = Double.parseDouble(parties[1]);
-            String villeB = parties[2];
-            Sommet b = new Sommet(villeB);
-            Resultat r = new Resultat(a,distance,b);
-            resultats.add(r);
-
+            String nomV = parties[2];
+            Sommet u = Sommet.getSommetAvecNom(sommets, nomU);
+            Sommet v = Sommet.getSommetAvecNom(sommets, nomV);
+            Arete a = Arete.getArete(aretes, u, v);
+            a.setDistance(distance);
+            if (a != null) {
+                areteResultats.add(a);
+            }
         }
-        return resultats;
+        return areteResultats;
+    }
+
+    static public ArrayList<Planete> chargerPlaneteDepuisFichier(String nomFichier) {
+        ArrayList<Planete> planetes = new ArrayList<>();
+        List<String> lignes;
+
+        try {
+            lignes = Files.readAllLines(Paths.get(nomFichier));
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Erreur planete chargement" + e.getMessage());
+        }
+        int nbPlanete = Integer.parseInt(lignes.getFirst());
+        for (int i = 1; i <= nbPlanete; i++) {
+            String nom = lignes.get(i);
+            Planete p = new Planete(nom);
+            planetes.add(p);
+        }
+        return planetes;
+
     }
 
 
